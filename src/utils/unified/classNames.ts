8bcba5f784757bbd096e7af8fbd2dc5496cd9844
KingOfTheAce2 <84872320+KingOfTheAce2@users.noreply.github.com>
2025-09-19 14:06:13 +0200

    Update classNames.ts

--- src/utils/unified/classNames.ts
+++ src/utils/unified/classNames.ts
@@ -12,4 +12,11 @@ export function cn(
   return classNames(...classes);
 }
 
+export function classNames(...classes: (string | undefined | null | false)[]): string {
+  return classes.filter(Boolean).join(' ');
+}
+
+export function cn(...classes: (string | undefined | null | false)[]): string {
+  return classNames(...classes);
+}
 export default classNames;
