4cd729686c4c6d60959cd3661e6aed3766515796
KingOfTheAce2 <84872320+KingOfTheAce2@users.noreply.github.com>
2025-09-19 14:50:52 +0200

    Fix plugin event handler typings

--- src/plugins/core/types.ts
+++ src/plugins/core/types.ts
@@ -111,11 +111,13 @@ export interface PluginUIAPI {
   removePanel(panelId: string): void;
 }
 
+export type PluginEventHandler = (...args: any[]) => void;
+
 export interface PluginEventAPI {
-  on(event: string, handler: Function): void;
-  off(event: string, handler: Function): void;
+  on(event: string, handler: PluginEventHandler): void;
+  off(event: string, handler: PluginEventHandler): void;
   emit(event: string, data?: any): void;
-  once(event: string, handler: Function): void;
+  once(event: string, handler: PluginEventHandler): void;
 }
 
 export interface PluginUtilsAPI {
